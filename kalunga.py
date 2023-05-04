32a5d000742bc79e8283b2bb690b60d30a706211
Luís Beck <125098484+LuisHBeck@users.noreply.github.com>
2023-05-04 11:06:58 -0300

    Add files via upload

---
+++ kalunga.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+class Scraping():
+    def __init__(self) -> None:
+        self.site = 'https://www.kalunga.com.br/depto/smartphones-telefonia/smartphones/sansung/8/1190/4592'
+
+        self.map = {
+            'product': {
+                'xpath': '/html/body/main/div/div[2]/div[3]/div[2]/div[2]/div/div/div[*x*]/div/div[2]/a/h2'
+            },
+            
+            'price': {
+                'xpath': '/html/body/main/div/div[2]/div[3]/div[2]/div[2]/div/div[1]/div[*x*]/div/div[2]/div[2]/span'
+            }
+        }
+
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        self.driver = webdriver.Chrome(options=options)
+
+    def open(self):
+        sleep(2)
+        self.driver.get(self.site)
+        sleep(2)
+
+        product = self.driver.find_element(By.XPATH, self.map['product']['xpath'].replace("*x*", "1")).text
+
+        price = self.driver.find_element(By.XPATH, self.map['price']['xpath'].replace("*x*", "1")).text
+
+        print(product)
+        print(price)
+
---
+++ main.py
@@ -0,0 +1,5 @@
+from kalunga import Scraping
+
+if __name__ == '__main__':
+    teste = Scraping()
+    teste.open()
